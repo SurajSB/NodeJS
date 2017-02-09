1e5320cb6c237584c20cd8128c6ffd76a1f82d18
SurajSB <balekundrisuraj@gmail.com>
2017-02-09 21:17:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 });
 
+app.get('/getData', function (req, res) {
+  res.send('{"name:"suraj"}')
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 });
