0d0094e4f922b2d4d299b6c275e3b3eef94efeab
SurajSB <balekundrisuraj@gmail.com>
2017-03-29 23:25:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World1!')
+  res.send('Hello World!')
 });
 
 app.get('/getData', function (req, res) {
