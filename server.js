10fd81db8eb2f04f753169a777b3972692a4d992
SurajSB <balekundrisuraj@gmail.com>
2017-03-31 15:53:39 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Congratulations you have successfully running the service!')
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.get('/getData', function (req, res) {
+  res.send('{"name:"suraj"}')
+});
+
+app.listen(3001, function () {
+  console.log('Example app listening on port 3001!')
 });
 
